7ee4e9959aff0c1a7e8565862cf0cb775b4dca30
Hadi Behgam <h.behgam@gmail.com>
2025-02-10 17:07:16 +0330

    Create header.tsx

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,11 @@
+import Menu from "../home/navbar";
+
+export default function HeaderLayout() {
+  return (
+    <header>
+      <nav className="container-home">
+        <Menu />
+      </nav>
+    </header>
+  );
+}
