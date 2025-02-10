807da704960f3846a3e46ca4cd8c38f93cba26db
Hadi Behgam <h.behgam@gmail.com>
2025-02-10 18:31:51 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import Hero from "@/components/front/home-page/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
+    <Hero />
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start container-home">
         <p>Main part</p>
       </main>
     </div>
+    </>
   );
 }
