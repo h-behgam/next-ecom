a6173141fb6694439c83549043603e53c6cd2f0c
Hadi Behgam <h.behgam@gmail.com>
2025-02-12 15:51:08 +0330

    Create best-selling-products.tsx

---
+++ src/components/front/home-page/best-selling-products.tsx
@@ -0,0 +1,46 @@
+import Container from '@/components/ui/container';
+import { IBestSellingProducts } from '@/types/products-type';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+export default function BestSellingProducts() {
+  const products: IBestSellingProducts[] = [
+    { id: 1, name: 'Product 1', image: '/assets/images/best1.webp', link: '#' },
+    { id: 2, name: 'Product 2', image: '/assets/images/best2.webp', link: '#' },
+    { id: 3, name: 'Product 3', image: '/assets/images/best3.webp', link: '#' },
+    { id: 4, name: 'Product 4', image: '/assets/images/best4.webp', link: '#' },
+  ];
+  return (
+    <Container>
+      <div className='flex flex-col gap-4'>
+        <h2 className='mb-2 text-center text-xl font-bold text-slate-700 md:text-2xl md:font-medium lg:mb-6'>
+          خرید بر اساس دسته بندی
+        </h2>
+        <div className='grid auto-cols-fr grid-cols-1 gap-4 lg:grid-cols-2'>
+          {products.map((product) => (
+            <Link
+              href={product.link}
+              key={product.id}
+              className='flex flex-col items-center text-slate-700'
+            >
+              <Image
+                src={product.image}
+                alt={product.name}
+                className='rounded-xl'
+                width={0}
+                height={0}
+                sizes='(min-width: 808px) 50vw, 100vw'
+                style={{
+                  objectFit: 'contain',
+                  width: '100%',
+                  height: 'auto',
+                }}
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
+}
