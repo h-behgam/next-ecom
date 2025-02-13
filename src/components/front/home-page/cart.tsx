8fb58efd936144976d865ec8fe98dd193a003e0e
Hadi Behgam <h.behgam@gmail.com>
2025-02-13 18:31:39 +0330

    Update cart.tsx

--- src/components/front/home-page/cart.tsx
+++ src/components/front/home-page/cart.tsx
@@ -1,32 +1,63 @@
 import { e2p, sp } from '@/lib/replace-number';
+import { cn } from '@/lib/tailwind-helper';
+import { Product } from '@/types/products-type';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
 
-export default function Cart() {
+type CartProps = Omit<Product, 'id' | 'name' | 'stock'> & {
+  isHomePage: boolean;
+};
+export default function Cart({
+  description,
+  price,
+  image,
+  isHomePage,
+}: CartProps) {
   return (
     <Link href={'#'}>
-      <div className='flex h-64 w-40 flex-col items-center rounded-md bg-slate-100 p-2 text-sm/5'>
-        <div className='mb-2 block h-32 w-32'>
+      <div
+        className={cn(
+          'flex h-80 w-48 flex-col items-center border-b border-l border-slate-100 p-2 text-sm/5',
+          {
+            'w-40': isHomePage,
+            'h-64': isHomePage,
+            'rounded-md': isHomePage,
+            'border-0': isHomePage,
+            'bg-slate-50': isHomePage
+          },
+        )}
+      >
+        <div className='mb-2 block h-36 w-36'>
           <Image
-            className='w-full object-contain'
-            src={'/assets/images/product1.webp'}
+            className='mx-auto object-contain'
+            src={image}
             alt='product picture'
-            width={128}
-            height={128}
+            width={isHomePage ? 128 : 236}
+            height={isHomePage ? 128 : 236}
           />
         </div>
+
         <div className='h-24'>
-          <p className='mb-4 text-slate-700'>
-            گوشی موبایل شیائومی مدل 14T دو سیم کارت ...
-          </p>
+          <p className='mb-4 text-slate-700'>{description}</p>
+          {!isHomePage && (
+            <div className='mb-3 flex items-center justify-end'>
+              <span className='ml-1'>{e2p(4.36)}</span>
+              <Image
+                src={'/assets/images/star-icon.svg'}
+                alt='star'
+                className=''
+                width={16}
+                height={16}
+              />
+            </div>
+          )}
           <div className='flex leading-3'>
             <div className='flex w-full items-center justify-between'>
               <p className='rounded-xl bg-red-700 px-2 pt-0.5 text-base text-slate-100'>
                 {'٪ ' + e2p(8)}
               </p>
               <div className='flex items-center'>
-                <p className=''>{sp(12000000)}</p>
+                <p className=''>{sp(price)}</p>
                 <p className='text-xs font-thin'>تومان</p>
               </div>
             </div>
