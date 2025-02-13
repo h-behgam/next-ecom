5060c108c950148594c480ae0ff060cf41ee4466
Hadi Behgam <h.behgam@gmail.com>
2025-02-13 18:31:42 +0330

    Update buy-by-category.tsx

--- src/components/front/home-page/buy-by-category.tsx
+++ src/components/front/home-page/buy-by-category.tsx
@@ -1,43 +1,16 @@
 import Container from '@/components/ui/container';
+import { categories } from '@/constants/categories';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function BuyByCategory() {
-  const categories: CategoriesType[] = [
-    { id: 1, name: 'موبایل', image: '/assets/images/mobile.png', link: '#' },
-    {
-      id: 2,
-      name: 'کالای دیجیتال',
-      image: '/assets/images/digitals.png',
-      link: '#',
-    },
-    {
-      id: 3,
-      name: 'آرایشی بهداشتی',
-      image: '/assets/images/health.png',
-      link: '#',
-    },
-    { id: 4, name: 'طلا و نقره', image: '/assets/images/gold.png', link: '#' },
-    {
-      id: 5,
-      name: 'ورزش و سفر',
-      image: '/assets/images/sports.png',
-      link: '#',
-    },
-    {
-      id: 6,
-      name: 'خانه و آشپزخانه',
-      image: '/assets/images/home.png',
-      link: '#',
-    },
-  ];
   return (
     <Container>
       <div className='flex flex-col gap-4'>
         <h2 className='mb-2 text-center text-xl font-bold text-slate-700 md:text-2xl md:font-medium lg:mb-6'>
           خرید بر اساس دسته بندی
         </h2>
-        <div className='grid grid-cols-[repeat(auto-fit,10rem)] place-content-center md:place-content-between gap-16 sm:gap-4'>
+        <div className='grid grid-cols-[repeat(auto-fit,_minmax(10rem,_1fr))] place-content-center gap-16 sm:gap-4 md:place-content-between'>
           {categories.map((category) => (
             <Link
               href={category.link}
