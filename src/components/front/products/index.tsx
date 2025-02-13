d2130ee48c4515eb92c956471279146a72430b0e
Hadi Behgam <h.behgam@gmail.com>
2025-02-13 18:35:04 +0330

    Update index.tsx

--- src/components/front/products/index.tsx
+++ src/components/front/products/index.tsx
@@ -1,22 +1,10 @@
-import { products } from '@/constants/products';
-import Cart from '../home-page/cart';
+import ProductsList from "./products-list";
+
 
 export default function ProductsTemplate() {
   return (
     <>
-      <div className='flex gap-x-1 justify-center mt-5'>
-        {products?.map((product) => {
-          return (
-            <Cart
-              description={product.description}
-              price={product.price}
-              image={product.image}
-              key={product.id}
-              isHomePage={false}
-            />
-          );
-        })}
-      </div>
+      <ProductsList />
     </>
   );
 }
