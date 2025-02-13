5e6d8bcb3e73881ca942a76522b14e2453569bc4
Hadi Behgam <h.behgam@gmail.com>
2025-02-13 18:31:13 +0330

    Create products.ts

---
+++ src/constants/products.ts
@@ -0,0 +1,18 @@
+import { IBestSellingProducts, Product } from "@/types/products-type";
+
+export const products:Product[] = [
+    {id:1,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
+    {id:2,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
+    {id:3,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
+    {id:4,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
+    {id:5,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
+    {id:6,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
+    {id:7,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
+]
+
+export const bestProducts: IBestSellingProducts[] = [
+    { id: 1, name: 'Product 1', image: '/assets/images/best1.webp', link: '#' },
+    { id: 2, name: 'Product 2', image: '/assets/images/best2.webp', link: '#' },
+    { id: 3, name: 'Product 3', image: '/assets/images/best3.webp', link: '#' },
+    { id: 4, name: 'Product 4', image: '/assets/images/best4.webp', link: '#' },
+  ];
