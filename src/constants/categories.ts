29b279125787334ad9cbfd2569c92d3f33748602
Hadi Behgam <h.behgam@gmail.com>
2025-02-13 18:31:16 +0330

    Create categories.ts

---
+++ src/constants/categories.ts
@@ -0,0 +1,30 @@
+import { CategoriesType } from '@/types/categories-type';
+
+export const categories: CategoriesType[] = [
+  { id: 1, name: 'موبایل', image: '/assets/images/mobile.png', link: '#' },
+  {
+    id: 2,
+    name: 'کالای دیجیتال',
+    image: '/assets/images/digitals.png',
+    link: '#',
+  },
+  {
+    id: 3,
+    name: 'آرایشی بهداشتی',
+    image: '/assets/images/health.png',
+    link: '#',
+  },
+  { id: 4, name: 'طلا و نقره', image: '/assets/images/gold.png', link: '#' },
+  {
+    id: 5,
+    name: 'ورزش و سفر',
+    image: '/assets/images/sports.png',
+    link: '#',
+  },
+  {
+    id: 6,
+    name: 'خانه و آشپزخانه',
+    image: '/assets/images/home.png',
+    link: '#',
+  },
+];
