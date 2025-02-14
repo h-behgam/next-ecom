a280e0702cb406c0513967b992773545b13b5f73
Hadi Behgam <h.behgam@gmail.com>
2025-02-14 14:31:36 +0330

    Create brands.tsx

---
+++ src/components/front/products/brands.tsx
@@ -0,0 +1,35 @@
+import Container from '@/components/ui/container'
+import { brands } from '@/constants/products'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Brands() {
+  return (
+    <Container>
+        <div className='grid grid-cols-[repeat(auto-fit,_minmax(10rem,10rem))] place-content-center gap-4 sm:gap-8 md:place-content-center'>
+          {brands.map((brand) => (
+            <Link
+              href={brand.link}
+              key={brand.id}
+              className='flex flex-col items-center text-slate-700'
+            >
+              <Image
+                src={brand.image}
+                alt={brand.name}
+                className='rounded-xl'
+                width={0}
+                height={0}
+                sizes='(min-width: 808px) 50vw, 100vw'
+                style={{
+                  objectFit: 'contain',
+                  width: '100%',
+                  maxWidth: '160px',
+                  height: 'auto',
+                }}
+              />
+            </Link>
+          ))}
+        </div>
+    </Container>
+  )
+}
