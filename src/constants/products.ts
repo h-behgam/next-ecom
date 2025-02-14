432dc4305a1d5114ce77e3e675f970314d97c26d
Hadi Behgam <h.behgam@gmail.com>
2025-02-14 14:31:28 +0330

    Update products.ts

--- src/constants/products.ts
+++ src/constants/products.ts
@@ -1,13 +1,13 @@
-import { IBestSellingProducts, Product } from "@/types/products-type";
+import { IBestSellingProducts, IBrands, Product } from "@/types/products-type";
 
 export const products:Product[] = [
-    {id:1,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
-    {id:2,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
-    {id:3,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
+    {id:1,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product2.webp'},
+    {id:2,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product3.webp'},
+    {id:3,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product4.webp'},
     {id:4,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
-    {id:5,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
-    {id:6,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
-    {id:7,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
+    {id:5,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product2.webp'},
+    {id:6,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product3.webp'},
+    {id:7,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product4.webp'},
 ]
 
 export const bestProducts: IBestSellingProducts[] = [
@@ -16,3 +16,11 @@ export const bestProducts: IBestSellingProducts[] = [
     { id: 3, name: 'Product 3', image: '/assets/images/best3.webp', link: '#' },
     { id: 4, name: 'Product 4', image: '/assets/images/best4.webp', link: '#' },
   ];
+
+  export const brands: IBrands[] = [
+    { id: 1, name: 'Brand 1', image: '/assets/images/ads-samsung.webp', link: '#' },
+    { id: 2, name: 'Brand 2', image: '/assets/images/ads-apple.webp', link: '#' },
+    { id: 3, name: 'Brand 3', image: '/assets/images/ads-honor.webp', link: '#' },
+    { id: 4, name: 'Brand 4', image: '/assets/images/ads-xiaomi.webp', link: '#' },
+    { id: 5, name: 'Brand 5', image: '/assets/images/ads-other.webp', link: '#' },
+]
