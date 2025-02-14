cfd69fb03e3c7c6f30d05fc8f10e2f42a23cb405
Hadi Behgam <h.behgam@gmail.com>
2025-02-14 14:43:31 +0330

    Update amazings.tsx

--- src/components/front/home-page/amazings.tsx
+++ src/components/front/home-page/amazings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Cart from './cart';
+import Cart from '../cart';
 import Image from 'next/image';
 import Link from 'next/link';
 import Container from '@/components/ui/container';
