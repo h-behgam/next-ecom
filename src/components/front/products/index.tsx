95ce640b46a0ea7ccd2a3cbb7c9c2be6ab759391
Hadi Behgam <h.behgam@gmail.com>
2025-02-14 14:31:34 +0330

    Update index.tsx

--- src/components/front/products/index.tsx
+++ src/components/front/products/index.tsx
@@ -1,9 +1,12 @@
+import Brands from "./brands";
 import ProductsList from "./products-list";
 
 
 export default function ProductsTemplate() {
   return (
     <>
+      <ProductsList />
+      <Brands />
       <ProductsList />
     </>
   );
