bdeb32bb704ca30abb379fb6f9076149e13d1943
Hadi Behgam <h.behgam@gmail.com>
2025-02-15 18:49:14 +0330

    Update products-list.tsx

--- src/components/front/products/products-list.tsx
+++ src/components/front/products/products-list.tsx
@@ -3,10 +3,11 @@ import Cart from '../cart';
 
 export default function ProductsList() {
   return (
-    <div className='mt-5 flex justify-center gap-x-1 flex-wrap mb-6 sm:mb-0'>
+    <div className='mb-6 mt-5 flex flex-wrap justify-center gap-x-1 sm:mb-0'>
       {products?.map((product) => {
         return (
           <Cart
+            id={product.id}
             description={product.description}
             price={product.price}
             image={product.image}
