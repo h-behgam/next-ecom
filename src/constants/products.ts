d07c2158c03d0a708b8189494b959ac183552536
Hadi Behgam <h.behgam@gmail.com>
2025-02-15 16:30:27 +0330

    Update products.ts

--- src/constants/products.ts
+++ src/constants/products.ts
@@ -1,26 +1,155 @@
-import { IBestSellingProducts, IBrands, Product } from "@/types/products-type";
+import { IBestSellingProducts, IBrands, Product } from '@/types/products-type';
 
-export const products:Product[] = [
-    {id:1,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product2.webp'},
-    {id:2,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product3.webp'},
-    {id:3,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product4.webp'},
-    {id:4,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product1.webp'},
-    {id:5,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product2.webp'},
-    {id:6,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product3.webp'},
-    {id:7,name: 'موبایل',description: 'گوشی موبایل شیائومی مدل 14T دو سیم کارت ...',price: 12000000,stock: 5,image: '/assets/images/product4.webp'},
-]
+export const products: Product[] = [
+  {
+    id: 1,
+    name: 'گوشی موبایل سامسونگ مدل Galaxy A55 دو سیم کارت ظرفیت 256 گیگابایت و رم 8 گیگابایت',
+    enName: 'Samsung Galaxy A55 Dual SIM 256GB And 8GB RAM Mobile Phone',
+    description:
+      'گوشی موبایل سامسونگ مدل Galaxy A55 نسخه‌ی به روزشده‌ی میان‌رده‌ی محبوب A54 محسوب می‌شود. این گوشی با بهره‌مندی از نمایشگر 6.6 اینچی Super AMOLED با کیفیت با نرخ تازه‌سازی 120 فریم برثانیه یکی از بهترین نمایشگرهای میانرده را دارد.',
+    price: 12000000,
+    stock: 5,
+    image: '/assets/images/product2.webp',
+    rate: 405,
+    features: [
+      { title: 'فناوری صفحه‌ نمایش', description: 'Super AMOLED' },
+      { title: 'نسخه سیستم عامل', description: 'Android 14' },
+      { title: 'رزولوشن دوربین اصلی', description: '50 مگاپیکسل' },
+      { title: 'اندازه', description: '6.6' },
+      { title: 'اقلام همراه', description: 'دفترچه‌ راهنما، کابل USB' },
+    ],
+  },
+  {
+    id: 2,
+    name: 'گوشی موبایل سامسونگ مدل Galaxy A55 دو سیم کارت ظرفیت 256 گیگابایت و رم 8 گیگابایت',
+    enName: 'Samsung Galaxy A55 Dual SIM 256GB And 8GB RAM Mobile Phone',
+    description:
+      'گوشی موبایل سامسونگ مدل Galaxy A55 نسخه‌ی به روزشده‌ی میان‌رده‌ی محبوب A54 محسوب می‌شود. این گوشی با بهره‌مندی از نمایشگر 6.6 اینچی Super AMOLED با کیفیت با نرخ تازه‌سازی 120 فریم برثانیه یکی از بهترین نمایشگرهای میانرده را دارد.',
+    price: 12000000,
+    stock: 5,
+    image: '/assets/images/product3.webp',
+    rate: 405,
+    features: [
+      { title: 'فناوری صفحه‌ نمایش', description: 'Super AMOLED' },
+      { title: 'نسخه سیستم عامل', description: 'Android 14' },
+      { title: 'رزولوشن دوربین اصلی', description: '50 مگاپیکسل' },
+      { title: 'اندازه', description: '6.6' },
+      { title: 'اقلام همراه', description: 'دفترچه‌ راهنما، کابل USB' },
+    ],
+  },
+  {
+    id: 3,
+    name: 'گوشی موبایل سامسونگ مدل Galaxy A55 دو سیم کارت ظرفیت 256 گیگابایت و رم 8 گیگابایت',
+    enName: 'Samsung Galaxy A55 Dual SIM 256GB And 8GB RAM Mobile Phone',
+    description:
+      'گوشی موبایل سامسونگ مدل Galaxy A55 نسخه‌ی به روزشده‌ی میان‌رده‌ی محبوب A54 محسوب می‌شود. این گوشی با بهره‌مندی از نمایشگر 6.6 اینچی Super AMOLED با کیفیت با نرخ تازه‌سازی 120 فریم برثانیه یکی از بهترین نمایشگرهای میانرده را دارد.',
+    price: 12000000,
+    stock: 5,
+    image: '/assets/images/product4.webp',
+    rate: 405,
+    features: [
+      { title: 'فناوری صفحه‌ نمایش', description: 'Super AMOLED' },
+      { title: 'نسخه سیستم عامل', description: 'Android 14' },
+      { title: 'رزولوشن دوربین اصلی', description: '50 مگاپیکسل' },
+      { title: 'اندازه', description: '6.6' },
+      { title: 'اقلام همراه', description: 'دفترچه‌ راهنما، کابل USB' },
+    ],
+  },
+  {
+    id: 4,
+    name: 'گوشی موبایل سامسونگ مدل Galaxy A55 دو سیم کارت ظرفیت 256 گیگابایت و رم 8 گیگابایت',
+    enName: 'Samsung Galaxy A55 Dual SIM 256GB And 8GB RAM Mobile Phone',
+    description:
+      'گوشی موبایل سامسونگ مدل Galaxy A55 نسخه‌ی به روزشده‌ی میان‌رده‌ی محبوب A54 محسوب می‌شود. این گوشی با بهره‌مندی از نمایشگر 6.6 اینچی Super AMOLED با کیفیت با نرخ تازه‌سازی 120 فریم برثانیه یکی از بهترین نمایشگرهای میانرده را دارد.',
+    price: 12000000,
+    stock: 5,
+    image: '/assets/images/product1.webp',
+    rate: 405,
+    features: [
+      { title: 'فناوری صفحه‌ نمایش', description: 'Super AMOLED' },
+      { title: 'نسخه سیستم عامل', description: 'Android 14' },
+      { title: 'رزولوشن دوربین اصلی', description: '50 مگاپیکسل' },
+      { title: 'اندازه', description: '6.6' },
+      { title: 'اقلام همراه', description: 'دفترچه‌ راهنما، کابل USB' },
+    ],
+  },
+  {
+    id: 5,
+    name: 'گوشی موبایل سامسونگ مدل Galaxy A55 دو سیم کارت ظرفیت 256 گیگابایت و رم 8 گیگابایت',
+    enName: 'Samsung Galaxy A55 Dual SIM 256GB And 8GB RAM Mobile Phone',
+    description:
+      'گوشی موبایل سامسونگ مدل Galaxy A55 نسخه‌ی به روزشده‌ی میان‌رده‌ی محبوب A54 محسوب می‌شود. این گوشی با بهره‌مندی از نمایشگر 6.6 اینچی Super AMOLED با کیفیت با نرخ تازه‌سازی 120 فریم برثانیه یکی از بهترین نمایشگرهای میانرده را دارد.',
+    price: 12000000,
+    stock: 5,
+    image: '/assets/images/product2.webp',
+    rate: 405,
+    features: [
+      { title: 'فناوری صفحه‌ نمایش', description: 'Super AMOLED' },
+      { title: 'نسخه سیستم عامل', description: 'Android 14' },
+      { title: 'رزولوشن دوربین اصلی', description: '50 مگاپیکسل' },
+      { title: 'اندازه', description: '6.6' },
+      { title: 'اقلام همراه', description: 'دفترچه‌ راهنما، کابل USB' },
+    ],
+  },
+  {
+    id: 6,
+    name: 'گوشی موبایل سامسونگ مدل Galaxy A55 دو سیم کارت ظرفیت 256 گیگابایت و رم 8 گیگابایت',
+    enName: 'Samsung Galaxy A55 Dual SIM 256GB And 8GB RAM Mobile Phone',
+    description:
+      'گوشی موبایل سامسونگ مدل Galaxy A55 نسخه‌ی به روزشده‌ی میان‌رده‌ی محبوب A54 محسوب می‌شود. این گوشی با بهره‌مندی از نمایشگر 6.6 اینچی Super AMOLED با کیفیت با نرخ تازه‌سازی 120 فریم برثانیه یکی از بهترین نمایشگرهای میانرده را دارد.',
+    price: 12000000,
+    stock: 5,
+    image: '/assets/images/product3.webp',
+    rate: 405,
+    features: [
+      { title: 'فناوری صفحه‌ نمایش', description: 'Super AMOLED' },
+      { title: 'نسخه سیستم عامل', description: 'Android 14' },
+      { title: 'رزولوشن دوربین اصلی', description: '50 مگاپیکسل' },
+      { title: 'اندازه', description: '6.6' },
+      { title: 'اقلام همراه', description: 'دفترچه‌ راهنما، کابل USB' },
+    ],
+  },
+  {
+    id: 7,
+    name: 'گوشی موبایل سامسونگ مدل Galaxy A55 دو سیم کارت ظرفیت 256 گیگابایت و رم 8 گیگابایت',
+    enName: 'Samsung Galaxy A55 Dual SIM 256GB And 8GB RAM Mobile Phone',
+    description:
+      'گوشی موبایل سامسونگ مدل Galaxy A55 نسخه‌ی به روزشده‌ی میان‌رده‌ی محبوب A54 محسوب می‌شود. این گوشی با بهره‌مندی از نمایشگر 6.6 اینچی Super AMOLED با کیفیت با نرخ تازه‌سازی 120 فریم برثانیه یکی از بهترین نمایشگرهای میانرده را دارد.',
+    price: 12000000,
+    stock: 5,
+    image: '/assets/images/product4.webp',
+    rate: 405,
+    features: [
+      { title: 'فناوری صفحه‌ نمایش', description: 'Super AMOLED' },
+      { title: 'نسخه سیستم عامل', description: 'Android 14' },
+      { title: 'رزولوشن دوربین اصلی', description: '50 مگاپیکسل' },
+      { title: 'اندازه', description: '6.6' },
+      { title: 'اقلام همراه', description: 'دفترچه‌ راهنما، کابل USB' },
+    ],
+  },
+];
 
 export const bestProducts: IBestSellingProducts[] = [
-    { id: 1, name: 'Product 1', image: '/assets/images/best1.webp', link: '#' },
-    { id: 2, name: 'Product 2', image: '/assets/images/best2.webp', link: '#' },
-    { id: 3, name: 'Product 3', image: '/assets/images/best3.webp', link: '#' },
-    { id: 4, name: 'Product 4', image: '/assets/images/best4.webp', link: '#' },
-  ];
+  { id: 1, name: 'Product 1', image: '/assets/images/best1.webp', link: '#' },
+  { id: 2, name: 'Product 2', image: '/assets/images/best2.webp', link: '#' },
+  { id: 3, name: 'Product 3', image: '/assets/images/best3.webp', link: '#' },
+  { id: 4, name: 'Product 4', image: '/assets/images/best4.webp', link: '#' },
+];
 
-  export const brands: IBrands[] = [
-    { id: 1, name: 'Brand 1', image: '/assets/images/ads-samsung.webp', link: '#' },
-    { id: 2, name: 'Brand 2', image: '/assets/images/ads-apple.webp', link: '#' },
-    { id: 3, name: 'Brand 3', image: '/assets/images/ads-honor.webp', link: '#' },
-    { id: 4, name: 'Brand 4', image: '/assets/images/ads-xiaomi.webp', link: '#' },
-    { id: 5, name: 'Brand 5', image: '/assets/images/ads-other.webp', link: '#' },
-]
+export const brands: IBrands[] = [
+  {
+    id: 1,
+    name: 'Brand 1',
+    image: '/assets/images/ads-samsung.webp',
+    link: '#',
+  },
+  { id: 2, name: 'Brand 2', image: '/assets/images/ads-apple.webp', link: '#' },
+  { id: 3, name: 'Brand 3', image: '/assets/images/ads-honor.webp', link: '#' },
+  {
+    id: 4,
+    name: 'Brand 4',
+    image: '/assets/images/ads-xiaomi.webp',
+    link: '#',
+  },
+  { id: 5, name: 'Brand 5', image: '/assets/images/ads-other.webp', link: '#' },
+];
