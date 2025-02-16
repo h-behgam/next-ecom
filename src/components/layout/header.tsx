c8cd34d94767df3db74af8d46569ce9d45444098
Hadi Behgam <h.behgam@gmail.com>
2025-02-16 18:05:15 +0330

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,9 @@
-import Menu from "../front/navbar";
+import Menu from '../front/navbar';
 
 export default function HeaderLayout() {
   return (
     <header>
-      <nav className="container-home">
-        <Menu />
-      </nav>
+      <Menu />
     </header>
   );
 }
