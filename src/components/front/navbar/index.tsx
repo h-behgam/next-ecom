ef738dfaa2cfa3e117be90e1ef2bdd7c190386cf
Hadi Behgam <h.behgam@gmail.com>
2025-02-16 18:05:22 +0330

    Update index.tsx

--- src/components/front/navbar/index.tsx
+++ src/components/front/navbar/index.tsx
@@ -1,19 +1,11 @@
-import DesktopMenu from "./desktop-navbar";
-import Link from "next/link";
+import DesktopMenu from './desktop-navbar';
+import MobileNavbar from './mobile-navbar';
 
 export default function Menu() {
   return (
     <>
-      <div className="flex justify-between">
-        <DesktopMenu />
-        <ul>
-          <li>
-            <Link className="p-4 block" href={"/"}>
-              ورود
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <DesktopMenu />
+      <MobileNavbar />
     </>
   );
 }
