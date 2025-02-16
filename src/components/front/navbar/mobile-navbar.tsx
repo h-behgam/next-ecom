50103743bc67f78b814bcf04ab0fe9ab7fb4b2aa
Hadi Behgam <h.behgam@gmail.com>
2025-02-16 19:16:46 +0330

    Update mobile-navbar.tsx

--- src/components/front/navbar/mobile-navbar.tsx
+++ src/components/front/navbar/mobile-navbar.tsx
@@ -1,31 +1,43 @@
+'use client';
 import Image from 'next/image';
-import Link from 'next/link';
+import { useState } from 'react';
+import MobileItems from './mobile-items';
+import { cn } from '@/lib/tailwind-helper';
 
 export default function MobileNavbar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <div className='block h-full md:hidden'>
-      <header className='flex justify-between items-center p-4'>
-        <Image className='block' src={'/assets/images/menu-hamburger.svg'} alt='logo' width={30} height={30} />
-        <Image className='block' src={'/assets/images/user.svg'} alt='logo' width={30} height={30} />
+    <div className='relative block h-full md:hidden'>
+      <header className='flex items-center justify-between p-4'>
+        <a onClick={() => setIsOpen(!isOpen)}>
+          <Image
+            className='block'
+            src={'/assets/images/menu-hamburger.svg'}
+            alt='logo'
+            width={30}
+            height={30}
+          />
+        </a>
+        <a>
+          <Image
+            className='block'
+            src={'/assets/images/user.svg'}
+            alt='logo'
+            width={30}
+            height={30}
+          />
+        </a>
       </header>
-      <div>
-        <ul className='flex gap-x-3'>
-          <li>
-            <Link href={'/'} className='block p-4'>
-              خانه
-            </Link>
-          </li>
-          <li>
-            <Link href={'/products'} className='block p-4'>
-              محصولات
-            </Link>
-          </li>
-          <li>
-            <Link href={'/'} className='block p-4'>
-              تماس باما
-            </Link>
-          </li>
-        </ul>
+      {/* <div className={cn('absolute w-0 bg-slate-50 duration-500 h-dvh',
+        {'w-1/2': isOpen}
+      )}> */}
+      <div
+        className={cn(
+          `fixed left-0 h-dvh w-2/3 -translate-x-full transform bg-slate-50 shadow-lg transition-transform duration-500 ease-in-out`,
+          { 'translate-x-0': isOpen },
+        )}
+      >
+        {isOpen && <MobileItems isOpen />}
       </div>
     </div>
   );
