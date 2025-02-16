61380412c8fc865e425af58b9bce03b04c89a903
Hadi Behgam <h.behgam@gmail.com>
2025-02-16 18:05:28 +0330

    Update desktop-navbar.tsx

--- src/components/front/navbar/desktop-navbar.tsx
+++ src/components/front/navbar/desktop-navbar.tsx
@@ -1,23 +1,36 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function DesktopMenu() {
   return (
-    <ul className="flex gap-x-3">
-      <li>
-        <Link href={"/"} className="p-4 block">
-          خانه
-        </Link>
-      </li>
-      <li>
-        <Link href={"/products"} className="p-4 block">
-          محصولات
-        </Link>
-      </li>
-      <li>
-        <Link href={"/"} className="p-4 block">
-          تماس باما
-        </Link>
-      </li>
-    </ul>
+    <nav className='container-home hidden md:block'>
+      <div className='flex justify-between'>
+        <div className=''>
+          <ul className='flex gap-x-3'>
+            <li>
+              <Link href={'/'} className='block p-4'>
+                خانه
+              </Link>
+            </li>
+            <li>
+              <Link href={'/products'} className='block p-4'>
+                محصولات
+              </Link>
+            </li>
+            <li>
+              <Link href={'/'} className='block p-4'>
+                تماس باما
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <ul className='mr-auto'>
+          <li>
+            <Link className='block p-4' href={'/'}>
+              ورود
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
