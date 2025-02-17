d4bef87ee249c8cbc9838c9a9a449957b4479ccd
Hadi Behgam <h.behgam@gmail.com>
2025-02-17 16:42:58 +0330

    Update mobile-items.tsx

--- src/components/front/navbar/mobile-items.tsx
+++ src/components/front/navbar/mobile-items.tsx
@@ -1,35 +1,61 @@
+import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { MobileMenu } from './mobile-navbar';
 
-export default function MobileItems({ isOpen }: { isOpen: boolean }) {
-  console.log(isOpen);
-  // جلوگیری از اسکرول هنگام باز بودن منو
-  useEffect(() => {
-    if (isOpen) {
-      document.body.classList.add('overflow-hidden');
-    } else {
-      document.body.classList.remove('overflow-hidden');
-    }
+export default function MobileItems({ isOpen, menus, setIsOpen }: MobileMenu) {
+  // جلوگیری از اسکرول هنگام باز بودن منو با استفاده از useLayoutEffect
+  useLayoutEffect(() => {
+    document.body.classList.toggle('overflow-hidden', isOpen);
     // پاک‌سازی هنگام خروج از کامپوننت
-    return () => document.body.classList.remove('overflow-hidden');
+    return () => {
+      if (isOpen) document.body.classList.remove('overflow-hidden');
+    };
   }, [isOpen]);
+
+  // مدیریت وضعیت باز/بسته بودن ساب‌منوها
+  const [activeMenu, setActiveMenu] = useState<number | null>(null);
+
+  const toggleSubMenu = (id: number) => {
+    setActiveMenu((prev) => (prev === id ? null : id));
+  };
+
   return (
-    <ul className='flex flex-col gap-x-3'>
-      <li>
-        <Link href={'/'} className='block py-1 px-4'>
-          خانه
-        </Link>
-      </li>
-      <li>
-        <Link href={'/products'} className='block py-1 px-4'>
-          محصولات
-        </Link>
-      </li>
-      <li>
-        <Link href={'/'} className='block py-1 px-4'>
-          تماس باما
-        </Link>
-      </li>
+    <ul className='flex flex-col gap-x-3 divide-y'>
+      {menus?.map((menu) => (
+        <li key={menu.id} className='bg-slate-50'>
+          {menu.subMenu ? (
+            <div
+              className='flex cursor-pointer justify-between p-3'
+              onClick={() => toggleSubMenu(menu.id)}
+            >
+              {menu.name}
+              <Image
+                src='/assets/images/icons/chevron-down.svg'
+                alt='chevron-down'
+                width={20}
+                height={20}
+                className={`h-5 w-5 transform transition-transform ${activeMenu === menu.id ? 'rotate-180' : ''}`}
+              />
+            </div>
+          ) : (
+            <Link
+              href={menu.link}
+              onClick={() => setIsOpen(false)}
+              className='flex cursor-pointer justify-between p-3'
+            >
+              {menu.name}
+            </Link>
+          )}
+          {menu.subMenu && activeMenu === menu.id && (
+            <MobileItems
+              isOpen={isOpen}
+              menus={menu.subMenu}
+              setIsOpen={setIsOpen}
+            />
+          )}
+        </li>
+      ))}
     </ul>
   );
 }
