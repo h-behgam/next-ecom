19380fbd2ac3502dc87f9a7123dad12803545c52
Hadi Behgam <h.behgam@gmail.com>
2025-02-18 17:29:09 +0330

    Update index.tsx

--- src/components/front/navbar/index.tsx
+++ src/components/front/navbar/index.tsx
@@ -1,13 +1,15 @@
-import { menus } from '@/constants/menus';
+// import { menus } from '@/constants/menus';
 import DesktopMenu from './desktop-navbar';
 import MobileNavbar from './mobile-navbar';
+import { getAllMenus } from '@/actions/menu-action';
 
-export default function Menu() {
+export default async function Menu() {
+  const menuss = await getAllMenus();
 
   return (
     <>
-      <DesktopMenu menus={menus}/>
-      <MobileNavbar menus={menus}/>
+      <DesktopMenu menus={menuss} />
+      <MobileNavbar menus={menuss} />
     </>
   );
 }
