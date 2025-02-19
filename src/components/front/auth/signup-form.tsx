54bb6eff9015f82a282d04a05810a5ddf5777c59
Hadi Behgam <h.behgam@gmail.com>
2025-02-19 17:41:39 +0330

    Update signup-form.tsx

--- src/components/front/auth/signup-form.tsx
+++ src/components/front/auth/signup-form.tsx
@@ -1,73 +1,66 @@
-import Link from "next/link";
-import CustomInput from "../custom-input";
-import ClientButton from "../client-button";
+import Link from 'next/link';
+import CustomInput from '../custom-input';
+import ClientButton from '../client-button';
 
 export default function SignupTemplate() {
-    const formHandler = async (formData: FormData) => {}
+  const formHandler = async (formData: FormData) => {};
 
   return (
     <div className='mx-auto w-full max-w-md'>
-    <div className='mb-6 text-center'>
-      <h2 className='text-3xl font-bold'>Create an account</h2>
-      <p>Enter your information to get started</p>
+      <div className='mb-6 text-center'>
+        <h2 className='text-xl font-bold'>ابتدا ثبت نام کنید.</h2>
+        <p>لطفا فیلد ها رو به صورت صحیح وارد کنید</p>
+      </div>
+      <form
+        className=''
+        // action={formHandler}
+      >
+        <div className='mb-2 p-1'>
+          <CustomInput
+            name='username'
+            type='text'
+            placeholder='نام کاربری'
+            labalName='username'
+            labelTitle='نام کاربری'
+            autoComplete='autoComplete'
+            labelClassName='mb-1 block'
+          />
+        </div>
+        <div className='mb-2 p-1'>
+          <CustomInput
+            name='password'
+            type='text'
+            placeholder='رمز عبور'
+            labalName='password'
+            labelTitle='رمز عبور'
+            labelClassName='mb-1 block'
+            />
+        </div>
+        <div className='mb-2 p-1'>
+          <CustomInput
+            name='confirmPassword'
+            type='text'
+            placeholder='تکرار رمز عبور'
+            labalName='confirmPassword'
+            labelTitle='تکرار رمز عبور'
+            labelClassName='mb-1 block'
+          />
+        </div>
+        <div>
+          <ClientButton
+            className='w-full rounded-md bg-slate-50 p-3 font-bold'
+            disabled={false}
+          >
+            ثبت نام
+          </ClientButton>
+        </div>
+      </form>
+      <p className='mt-4 text-center'>
+        از قبل اکانت دارید؟{' '}
+        <span className='font-semibold text-blue-800'>
+          <Link href={'/signin'}>ورود</Link>
+        </span>
+      </p>
     </div>
-    <form className='' action={formHandler}>
-      <div className='mb-2 p-1'>
-        <CustomInput
-          name='name'
-          type='text'
-          placeholder='Name'
-          labalName='name'
-          labelTitle='Name'
-          autoComplete='autoComplete'
-        />
-      </div>
-      <div className='mb-2 p-1'>
-        <CustomInput
-          name='username'
-          type='text'
-          placeholder='Username'
-          labalName='username'
-          labelTitle='Username'
-          autoComplete='autoComplete'
-        />
-      </div>
-      <div className='mb-2 p-1'>
-        <CustomInput
-          name='email'
-          type='text'
-          placeholder='Email'
-          labalName='email'
-          labelTitle='Email'
-          autoComplete='autoComplete'
-        />
-      </div>
-      <div className='mb-2 p-1'>
-        <CustomInput
-          name='password'
-          type='text'
-          placeholder='Password'
-          labalName='password'
-          labelTitle='Password'
-        />
-      </div>
-      <div className='mb-2 p-1'>
-        <CustomInput
-          name='confirmPassword'
-          type='text'
-          placeholder='Confirm Password'
-          labalName='confirmPassword'
-          labelTitle='Confirm Password'
-        />
-
-      </div>
-      <div>
-        <ClientButton disabled={false}>Sign up</ClientButton>
-      </div>
-    </form>
-    <p className='mt-4 text-center'>
-      Already have an account? <Link href={'/signin'}>Login</Link>{' '}
-    </p>
-  </div>
-  )
+  );
 }
