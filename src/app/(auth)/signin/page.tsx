335994ca2e001bbe83ab5d5c6631b8edfcba4d29
Hadi Behgam <h.behgam@gmail.com>
2025-02-19 17:03:56 +0330

    Create page.tsx

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+import SigninTemplate from "@/components/front/auth/signin-form";
+
+export default function Signin() {
+  return (
+    <div className="container-home flex items-center justify-center py-20">
+      <SigninTemplate />
+    </div>
+  )
+}
