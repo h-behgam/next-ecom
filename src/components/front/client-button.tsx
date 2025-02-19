e3d00be5913306775258f03740ba257f720e17ef
Hadi Behgam <h.behgam@gmail.com>
2025-02-19 17:03:43 +0330

    Create client-button.tsx

---
+++ src/components/front/client-button.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { ReactNode } from 'react';
+
+interface InfoButtonProps {
+  children: ReactNode;
+  disabled: boolean;
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function ClientButton({
+  children,
+  disabled,
+  className,
+  onClick,
+}: InfoButtonProps) {
+  const defaultClassName =
+    'w-full rounded-sm bg-slate-50 px-5 py-2 text-slate-700 duration-300 hover:bg-slate-100';
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={className ? className : defaultClassName}
+    >
+      {children}
+    </button>
+  );
+}
