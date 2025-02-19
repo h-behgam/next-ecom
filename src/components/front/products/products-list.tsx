6a8df2624279b9548bba3075e19c785f95404407
Hadi Behgam <h.behgam@gmail.com>
2025-02-19 16:43:27 +0330

    Update products-list.tsx

--- src/components/front/products/products-list.tsx
+++ src/components/front/products/products-list.tsx
@@ -1,9 +1,13 @@
-import { products } from '@/constants/products';
 import Cart from '../cart';
 
-export default function ProductsList() {
+import { getAllProducts } from '@/actions/product-action';
+
+export default async function ProductsList() {
+  const allProducts = await getAllProducts();
+  const { products, error } = allProducts;
   return (
     <div className='mb-6 mt-5 flex flex-wrap justify-center gap-x-1 sm:mb-0'>
+      {error && <p className='text-red-600'>محصولی یافت نشد</p>}
       {products?.map((product) => {
         return (
           <Cart
