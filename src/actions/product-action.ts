e9f0c1ba4f2ae1863276676f08b8eab3ef4e0cb4
Hadi Behgam <h.behgam@gmail.com>
2025-02-19 14:03:19 +0330

    Update product-action.ts

--- src/actions/product-action.ts
+++ src/actions/product-action.ts
@@ -7,9 +7,23 @@ export const getAllProducts = async () => {
     if (!products)
       return { error: { status: 403, message: 'Products not exist !!!' } };
 
-    return products;
+    return { products };
   } catch (error) {
     console.log('error of getAllProducts is:', error);
+    return { error: { status: 500, message: 'خطای سرور' } };
+  }
+};
+
+export const getProductByID = async (id: number) => {
+  try {
+    const product = await PrismaDB.products.findFirst({ where: { id } });
+
+    if (!product)
+      return { error: { status: 403, message: 'product not exist !!!' } };
+
+    return { product };
+  } catch (error) {
+    console.log('error of getProductByID is:', error);
     return { error: { status: 500, message: 'خطای سرور' } };
   }
 };
