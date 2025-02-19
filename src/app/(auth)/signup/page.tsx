9724edbca727b2c4e4f3b5b6687f4ee318a310a1
Hadi Behgam <h.behgam@gmail.com>
2025-02-19 17:41:44 +0330

    Update page.tsx

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,7 +2,7 @@ import SignupTemplate from "@/components/front/auth/signup-form";
 
 export default function Signup() {
   return (
-    <div>
+    <div className="container-home flex items-center justify-center py-20">
       <SignupTemplate />
     </div>
   )
