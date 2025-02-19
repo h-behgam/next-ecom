8298fb762284b9122721af6cbfc7eb464156db3f
Hadi Behgam <h.behgam@gmail.com>
2025-02-19 12:52:34 +0330

    Create category-action.ts

---
+++ src/actions/category-action.ts
@@ -0,0 +1,15 @@
+import PrismaDB from '@/lib/PrismaDB';
+
+export const getAllCategories = async () => {
+  try {
+    const categories = await PrismaDB.categories.findMany();
+
+    if (!categories)
+      return { error: { status: 403, message: 'Categories not exist !!!' } };
+
+    return categories;
+  } catch (error) {
+    console.log('error of getAllMenus is:', error);
+    return { error: { status: 500, message: 'خطای سرور' } };
+  }
+};
