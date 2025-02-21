409eb2c5e162aefd0be2dbf4e4345f7501ec1aca
Hadi Behgam <h.behgam@gmail.com>
2025-02-21 14:17:11 +0330

    Create auth.ts

---
+++ src/zod/auth.ts
@@ -0,0 +1,20 @@
+import { string, z } from "zod";
+
+export const SignupUser = z.object({
+    username: string({required_error: 'نام کاربری باید وارد شود'}).min(4).trim(),
+    password: string().min(2).trim(),
+    confirmPassword: string().min(2).trim(),
+}).superRefine((val, ctx) => {
+    if (val.password !== val.confirmPassword) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: 'رمزعبور و تکرار آن با هم برابر نیستند!!!',
+            path: ['confirm']
+        })
+    }
+})
+
+export const SigninUser = z.object({
+    username: string({required_error: 'نام کاربری باید وارد شود'}).min(4,'نام کاربری باید حداقل 4 حرف باشد.').trim(),
+    password: string().min(2).trim(),
+})
