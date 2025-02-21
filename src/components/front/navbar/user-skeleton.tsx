aa6bccc37c4dae26d4dcc35db047a37764e4f6dc
Hadi Behgam <h.behgam@gmail.com>
2025-02-21 18:56:16 +0330

    Create user-skeleton.tsx

---
+++ src/components/front/navbar/user-skeleton.tsx
@@ -0,0 +1,7 @@
+export default function UserSkeleton() {
+  return (
+    <div role='status' className='max-w-sm animate-pulse'>
+      <div className='h-10 w-32 rounded-md bg-gray-200 dark:bg-gray-700'></div>
+    </div>
+  );
+}
