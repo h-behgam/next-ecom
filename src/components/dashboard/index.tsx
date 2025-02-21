5a37118bd3d8ab521fdcf1026c3ef77b888b2b74
Hadi Behgam <h.behgam@gmail.com>
2025-02-21 18:56:28 +0330

    Create index.tsx

---
+++ src/components/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function DashboardTemplate() {
+  return (
+    <div className='p20 md:min-h-96'>
+      <h1 className='text-4xl'>به پنل مدیریتی خوش آمدید</h1>
+    </div>
+  );
+}
