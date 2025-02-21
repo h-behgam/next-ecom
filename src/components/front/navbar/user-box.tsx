500729eeb7435ebbc494fab9307f7da2508457cb
Hadi Behgam <h.behgam@gmail.com>
2025-02-21 19:25:15 +0330

    Update user-box.tsx

--- src/components/front/navbar/user-box.tsx
+++ src/components/front/navbar/user-box.tsx
@@ -1,10 +1,10 @@
-"use client"; // اجباری برای کامپوننت‌های کلاینتی
+'use client'; // اجباری برای کامپوننت‌های کلاینتی
 
-import { signOut } from "next-auth/react";
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useEffect, useState, useRef, useCallback } from "react";
+import { signOut } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState, useRef, useCallback } from 'react';
 
 export default function UserBox({ name }: { name: string }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,47 +23,62 @@ export default function UserBox({ name }: { name: string }) {
     setIsOpen(false);
 
     // اضافه کردن و حذف رویداد کلیک خارج از منو
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside);
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [pathname, handleClickOutside]);
 
   return (
-    <div className="relative p-2" ref={menuRef}>
+    <div className='relative p-2' ref={menuRef}>
       <button
-        className="flex items-center justify-end"
+        className='flex items-center justify-end'
         onClick={() => setIsOpen((prev) => !prev)}
       >
-        <Image src="/assets/images/user.svg" alt="User Icon" width={30} height={30} />
         <Image
-          src="/assets/images/icons/chevron-down.svg"
-          alt="Chevron"
+          src='/assets/images/user.svg'
+          alt='User Icon'
+          width={30}
+          height={30}
+        />
+        <Image
+          src='/assets/images/icons/chevron-down.svg'
+          alt='Chevron'
           width={20}
           height={20}
-          className="transition-transform h-5 w-5 transform"
+          className='h-5 w-5 transform transition-transform'
         />
       </button>
 
       {isOpen && (
-        <div className="absolute left-0 w-52 rounded-lg p-6 shadow-lg bg-white">
-          <ul className="divide-y">
-            <li className="pb-3">{name}</li>
-            <li className="py-3">
-              <Link className="flex items-center gap-x-6" href="/dashboard">
-                <Image src="/assets/images/icons/admin.svg" alt="Dashboard" width={24} height={24} />
+        <div className='absolute left-0 w-52 rounded-lg bg-white p-6 shadow-lg'>
+          <ul className='divide-y'>
+            <li className='pb-3'>{name}</li>
+            <li className='py-3'>
+              <Link className='flex items-center gap-x-6' href='/dashboard'>
+                <Image
+                  src='/assets/images/icons/admin.svg'
+                  alt='Dashboard'
+                  width={24}
+                  height={24}
+                />
                 پنل مدیریت
               </Link>
             </li>
-            <li className="pt-3">
+            <li className='pt-3'>
               <button
-                className="flex items-center gap-x-6 w-full text-left"
+                className='flex w-full items-center gap-x-6 text-left'
                 onClick={() => {
                   setIsOpen(false);
                   signOut();
                 }}
               >
-                <Image src="/assets/images/icons/signout.svg" alt="Sign Out" width={24} height={24} />
+                <Image
+                  src='/assets/images/icons/signout.svg'
+                  alt='Sign Out'
+                  width={24}
+                  height={24}
+                />
                 خروج
               </button>
             </li>
@@ -73,10 +88,6 @@ export default function UserBox({ name }: { name: string }) {
     </div>
   );
 }
-
-
-
-
 
 // import { signOut } from 'next-auth/react';
 // import Image from 'next/image';
@@ -91,7 +102,7 @@ export default function UserBox({ name }: { name: string }) {
 //   const userBpxHandler = () => {
 //     setIsOpen(!isOpen);
 //   };
-  
+
 //   return (
 //     <div className='relative p-2'>
 //       <a className='flex items-center justify-end' onClick={userBpxHandler}>
