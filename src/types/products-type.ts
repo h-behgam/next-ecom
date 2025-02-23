aaec403bfe89e4ae4013ea84bfb0e79f9676bbc5
Hadi Behgam <h.behgam@gmail.com>
2025-02-23 17:04:16 +0330

    Update products-type.ts

--- src/types/products-type.ts
+++ src/types/products-type.ts
@@ -1,3 +1,5 @@
+import { Prisma } from "@prisma/client";
+
 export type Product = {
   id: number;
   name: string;
@@ -7,7 +9,7 @@ export type Product = {
   price: number;
   stock: number;
   rate: number;
-  features: { title: string; description: string }[];
+  features: { title: string; description: string }[]| Prisma.JsonValue;
 };
 
 export interface IBestSellingProducts {
@@ -23,3 +25,18 @@ export interface IBrands {
   image: string;
   link: string;
 }
+
+export type ProductImageProps = Omit<
+Product,
+'id' | 'stock' | 'features' | 'rate' | 'enName' | 'description' | 'price'
+>;
+
+export type AddToCartProps = Omit<
+Product,
+'id' | 'stock' | 'features' | 'rate' | 'enName' | 'description'
+>;
+
+export type ProductDetailsProps = Omit<
+Product,
+'id' | 'stock'   | 'image' 
+>;
