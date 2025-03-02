f6b279475ef7c6015a2bc352e2c56703bd6b8660
Hadi Behgam <h.behgam@gmail.com>
2025-03-02 15:42:45 +0330

    Update DashboardMain.tsx

--- src/components/layout/DashboardMain.tsx
+++ src/components/layout/DashboardMain.tsx
@@ -1,6 +1,8 @@
-
-export default function DashboardMain() {
+type DashboardMainProps = {
+  children: React.ReactNode;
+}
+export default function DashboardMain({children}:DashboardMainProps) {
   return (
-    <div>DashboardMain</div>
+    <main className="bg-slate-100 w-full p-2">{children}</main>
   )
 }
