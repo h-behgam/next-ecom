da9a91e6bd9acc8b18faaaca2d3e761533511910
Hadi Behgam <h.behgam@gmail.com>
2025-03-02 15:50:25 +0330

    Update DashboardMain.tsx

--- src/components/layout/DashboardMain.tsx
+++ src/components/layout/DashboardMain.tsx
@@ -3,6 +3,6 @@ type DashboardMainProps = {
 }
 export default function DashboardMain({children}:DashboardMainProps) {
   return (
-    <main className="bg-slate-100 w-full p-2">{children}</main>
+    <main className="bg-slate-50/25 w-full px-2 pt-10">{children}</main>
   )
 }
