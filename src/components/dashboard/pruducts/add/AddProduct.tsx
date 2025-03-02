839cd6088ad8ecae1e85e1ae7b773537224b4997
Hadi Behgam <h.behgam@gmail.com>
2025-03-02 20:21:27 +0330

    Create AddProduct.tsx

---
+++ src/components/dashboard/pruducts/add/AddProduct.tsx
@@ -0,0 +1,12 @@
+import AddForm from './AddForm';
+
+export default function AddProductTemplate() {
+  return (
+    <div>
+      <div>
+        <h1 className='mb-10 text-xl font-semibold'>اطلاعات محصول</h1>
+      </div>
+      <AddForm />
+    </div>
+  );
+}
