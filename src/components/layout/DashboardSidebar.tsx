431a8132d8a618a6f49dab7dc92292fcc9224070
Hadi Behgam <h.behgam@gmail.com>
2025-03-02 15:50:28 +0330

    Update DashboardSidebar.tsx

--- src/components/layout/DashboardSidebar.tsx
+++ src/components/layout/DashboardSidebar.tsx
@@ -1,14 +1,20 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function DashboardSidebar() {
   return (
-    <aside className="p-2 bg-slate-50/50 w-64 divide-y">
-      <h2 className="text-xl text-center mb-4">مدیریت</h2>
-    <nav>
-      <ul>
-        <li className="py-2"><Link className="font-semibold" href={'/dashboard/pruduct'}>محصولات</Link></li>
-      </ul>
-    </nav>
+    <aside className='w-64 divide-y bg-slate-50/50 p-2'>
+      <Link href={'/dashboard'}>
+        <h2 className='mb-4 text-center text-xl'>مدیریت</h2>
+      </Link>
+      <nav>
+        <ul>
+          <li className='py-2'>
+            <Link className='font-semibold' href={'/dashboard/pruduct/add'}>
+              افزودن محصول
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </aside>
-  )
+  );
 }
