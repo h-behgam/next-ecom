04abd39ee4acda8200b1fe8c1f8c1a92aa613ef0
Hadi Behgam <h.behgam@gmail.com>
2025-03-02 15:56:53 +0330

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,18 +1,23 @@
 import './../globals.css';
 
 import DashboardLayout from '@/components/layout/DashboardLayout';
+import { auth } from '@/lib/auth/auth';
 import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'فروشگاه گجت و لوازم الکترونیکی جدید',
   description: 'ما بهترین تجربه را برای خرید الکترونیکی ارائه می دهیم',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+  if (!session?.user) redirect('/signin');
+
   return (
     <html lang='en' dir='rtl'>
       <body className={`font-iranSans antialiased`}>
