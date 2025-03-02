1b8f39b8613fe4749a00da6944cc50afd4ce0bf0
Hadi Behgam <h.behgam@gmail.com>
2025-03-02 20:21:15 +0330

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,11 @@
 import NextAuth from 'next-auth';
 import authConfig from './lib/auth/auth.config';
-import { NextRequest } from 'next/server';
+// import { NextRequest } from 'next/server';
 
-// export const { auth: middleware } = NextAuth(authConfig);
-const { auth } = NextAuth(authConfig)
-export default auth(async function middleware(req: NextRequest) {
-  // Your custom middleware logic goes here
-})
+export const { auth: middleware } = NextAuth(authConfig);
+// const { auth } = NextAuth(authConfig)
+// export default auth(async function middleware(req: NextRequest) {
+// })
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
