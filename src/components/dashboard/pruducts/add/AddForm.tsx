b620ff7ab3e80c7d46c8af8b068ec414b2688f84
Hadi Behgam <h.behgam@gmail.com>
2025-03-02 20:21:36 +0330

    Create AddForm.tsx

---
+++ src/components/dashboard/pruducts/add/AddForm.tsx
@@ -0,0 +1,48 @@
+import { createProduct } from '@/actions/product-action';
+import CustomInput from '@/components/front/custom-input'
+import SubmitFormButton from '@/components/front/submit-form-button'
+import { revalidatePath } from 'next/cache';
+
+export default function AddForm() {
+  const formHandler = async (formData:FormData) => { 
+    'use server'
+    const {error} = await createProduct(formData)
+    if (!error)
+      revalidatePath('/products')
+    
+    
+   }
+  return (
+    <div className="">
+      <form action={formHandler}>
+      <div className="mb-6">
+        <CustomInput name={'name'} type='string' labelTitle='نام محصول' labalName='name'/>
+      </div>
+      <div className="mb-6">
+        <CustomInput name={'enName'} type='string' labelTitle='نام انگلیسی محصول' labalName='enName'/>
+      </div>
+      <div className="mb-6">
+        <CustomInput name={'description'} type='string' labelTitle='توضیحات' labalName='description'/>
+      </div>
+      <div className="mb-6">
+        <CustomInput name={'image'} type='string' labelTitle='آدرس عکس' labalName='image'/>
+      </div>
+      <div className="mb-6">
+        <CustomInput name={'price'} type='string' labelTitle='قیمت' labalName='price'/>
+      </div>
+      <div className="mb-6">
+        <CustomInput name={'stock'} type='string' labelTitle='موجودی' labalName='stock'/>
+      </div>
+      <div className="mb-6">
+        <CustomInput name={'rate'} type='string' labelTitle='امتیاز' labalName='rate'/>
+      </div>
+      <div className="mb-6">
+        <CustomInput name={'features'} type='string' labelTitle='ویژگی ها' labalName='features'/>
+      </div>
+      <div className="mt-10">
+        <SubmitFormButton classname={'rounded-md bg-slate-400 w-full'}>ثبت</SubmitFormButton>
+      </div>
+    </form>
+    </div>
+  )
+}
