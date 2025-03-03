224111b9c9ee255e8eb3f370ec1bee40c6e88353
Hadi Behgam <h.behgam@gmail.com>
2025-03-03 17:16:31 +0330

    remove redundant font

--- src/app/(front)/layout.tsx
+++ src/app/(front)/layout.tsx
@@ -1,21 +1,10 @@
 import './../globals.css';
 
-import { Geist, Geist_Mono } from 'next/font/google';
 import type { Metadata } from 'next';
 
 import ContextProvidersLayout from '@/providers/ContextProviders';
 
 import FrontLayout from '@/components/layout/FrontLayout';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'فروشگاه گجت و لوازم الکترونیکی جدید',
@@ -29,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' dir='rtl'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} font-iranSans antialiased`}
-      >
+      <body className={`font-iranSans antialiased`}>
         <ContextProvidersLayout>
           <FrontLayout>{children}</FrontLayout>
         </ContextProvidersLayout>
