498d4579cdbf48103fe1e296fdd18d6d21a9e1b7
Hadi Behgam <h.behgam@gmail.com>
2025-03-03 17:16:35 +0330

    Create sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+import { getAllProducts } from '@/actions/product-action';
+
+export default async function sitemap() {
+  try {
+    const products = (await getAllProducts()).products || [];
+    const dynamicRoutes = products.map((product) => ({
+      url: `http://localhost:3000/products/${product.id}`,
+      lastModified: new Date().toDateString(),
+    }));
+
+    const staticRoutes = ['', 'products', 'signin', 'signup'];
+    const routes = staticRoutes.map((route) => ({
+      url: `http://localhost:3000/${route}`,
+      lastModified: new Date().toDateString(),
+    }));
+    return [...routes, ...dynamicRoutes];
+  } catch (error) {
+    return [];
+  }
+}
