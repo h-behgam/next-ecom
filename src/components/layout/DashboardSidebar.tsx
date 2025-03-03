9ceba2460f0fa2de6e79bfd6106fc7a8a951cef3
Hadi Behgam <h.behgam@gmail.com>
2025-03-03 15:43:18 +0330

    Update DashboardSidebar.tsx

--- src/components/layout/DashboardSidebar.tsx
+++ src/components/layout/DashboardSidebar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function DashboardSidebar() {
   return (
-    <aside className='w-64 divide-y bg-slate-50/50 p-2'>
+    <aside className='w-full md:w-64 divide-y bg-slate-50/50 p-2 border-b-4 md:border-b-0'>
       <Link href={'/dashboard'}>
         <h2 className='mb-4 text-center text-xl'>مدیریت</h2>
       </Link>
