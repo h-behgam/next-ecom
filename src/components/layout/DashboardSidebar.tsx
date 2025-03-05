411c184c49480afecfcdc252c51debfc039d7a46
Hadi Behgam <h.behgam@gmail.com>
2025-03-05 13:36:59 +0330

    Update DashboardSidebar.tsx

--- src/components/layout/DashboardSidebar.tsx
+++ src/components/layout/DashboardSidebar.tsx
@@ -3,18 +3,23 @@ import Link from 'next/link';
 export default function DashboardSidebar() {
   return (
     <aside className='w-full md:w-64 divide-y bg-slate-50/50 p-2 border-b-4 md:border-b-0'>
-      <Link href={'/dashboard'}>
-        <h2 className='mb-4 text-center text-xl'>مدیریت</h2>
+      <Link href={'/'}>
+        <h2 className='mb-4 font-medium'>صفحه اصلی</h2>
       </Link>
-      <nav>
-        <ul>
-          <li className='py-2'>
-            <Link className='font-semibold' href={'/'}>
-              بازگشست به صفحه اصلی
+      <nav className=''>
+        <ul className='divide-y'>
+          <li className='pt-1 pb-2'>
+            <Link className='font-semibold text-sm' href={'/dashboard'}>
+            مدیریت
             </Link>
           </li>
-          <li className='py-2'>
-            <Link className='font-semibold' href={'/dashboard/pruduct/add'}>
+          <li className='pt-1 pb-2'>
+            <Link className='font-semibold text-sm' href={'/dashboard/pruduct'}>
+              محصولات
+            </Link>
+          </li>
+          <li className='pt-1 pb-2'>
+            <Link className='font-semibold text-sm' href={'/dashboard/pruduct/add'}>
               افزودن محصول
             </Link>
           </li>
