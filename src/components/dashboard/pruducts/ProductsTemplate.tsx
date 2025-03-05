bd20684172bc1e6f428fd69537f3f316bc900c59
Hadi Behgam <h.behgam@gmail.com>
2025-03-05 19:25:29 +0330

    Update ProductsTemplate.tsx

--- src/components/dashboard/pruducts/ProductsTemplate.tsx
+++ src/components/dashboard/pruducts/ProductsTemplate.tsx
@@ -2,8 +2,8 @@ import ProductsTable from './ProductsTable';
 
 export default function DashboardProductsTemplate() {
   return (
-    <div className=''>
-      <div>
+    <div className='m-0 mt-2 rounded-lg border p-0 md:m-4 md:p-4'>
+      <div className='p-2 md:p-4'>
         <h2 className='text-xl font-semibold'>نمایش محصولات</h2>
       </div>
       <ProductsTable />
