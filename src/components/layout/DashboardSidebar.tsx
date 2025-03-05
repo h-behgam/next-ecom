7225b96d275ca9f2a75fe296ba19a70a5d5431fd
Hadi Behgam <h.behgam@gmail.com>
2025-03-05 19:25:19 +0330

    Update DashboardSidebar.tsx

--- src/components/layout/DashboardSidebar.tsx
+++ src/components/layout/DashboardSidebar.tsx
@@ -1,25 +1,45 @@
 import Link from 'next/link';
+import { FaHome } from 'react-icons/fa';
+import { AiOutlineProduct } from 'react-icons/ai';
+import { RxDashboard } from 'react-icons/rx';
+import { CgAddR } from 'react-icons/cg';
 
 export default function DashboardSidebar() {
   return (
-    <aside className='w-full md:w-64 divide-y bg-slate-50/50 p-2 border-b-4 md:border-b-0'>
-      <Link href={'/'}>
-        <h2 className='mb-4 font-medium'>صفحه اصلی</h2>
+    <aside className='w-full border-b-4 border-l bg-slate-50/50 md:w-64 md:border-b-0'>
+      <Link
+        className='flex h-16 items-center gap-x-2 border-b px-2 py-4'
+        href={'/'}
+      >
+        <FaHome size={20} />
+        <h2 className='font-medium'>صفحه اصلی</h2>
       </Link>
-      <nav className=''>
-        <ul className='divide-y'>
-          <li className='pt-1 pb-2'>
-            <Link className='font-semibold text-sm' href={'/dashboard'}>
-            مدیریت
+      <nav className='pt-5'>
+        <ul className='divide-yl'>
+          <li className='pb-2 pt-1 duration-300 hover:bg-slate-100'>
+            <Link
+              className='flex items-center gap-x-2 px-2 py-1 text-sm font-semibold'
+              href={'/dashboard'}
+            >
+              <RxDashboard size={20} />
+              مدیریت
             </Link>
           </li>
-          <li className='pt-1 pb-2'>
-            <Link className='font-semibold text-sm' href={'/dashboard/pruduct'}>
+          <li className='pb-2 pt-1 duration-300 hover:bg-slate-100'>
+            <Link
+              className='flex items-center gap-x-2 px-2 py-1 text-sm font-semibold'
+              href={'/dashboard/pruduct'}
+            >
+              <AiOutlineProduct size={20} />
               محصولات
             </Link>
           </li>
-          <li className='pt-1 pb-2'>
-            <Link className='font-semibold text-sm' href={'/dashboard/pruduct/add'}>
+          <li className='pb-2 pt-1 duration-300 hover:bg-slate-100'>
+            <Link
+              className='flex items-center gap-x-2 px-2 py-1 text-sm font-semibold'
+              href={'/dashboard/pruduct/add'}
+            >
+              <CgAddR size={20} />
               افزودن محصول
             </Link>
           </li>
