a771baef21f71f52f592c76103d371e676f000b2
Hadi Behgam <h.behgam@gmail.com>
2025-03-31 18:18:59 +0330

    Create AddToCartButton.tsx

---
+++ src/components/front/product/AddToCartButton.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { cn } from '@/lib/tailwind-helper';
+import { useFormStatus } from 'react-dom';
+
+export default function AddToCartButton({
+  classname,
+  children,
+  onClick,
+}: {
+  // classname?: React.CSSProperties;
+  classname?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode;
+}) {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      type='submit'
+      disabled={pending}
+      onClick={onClick}
+      className={cn(
+        'bg-second_color hover:bg-hover_color flex items-center justify-center gap-x-4 rounded-lg px-7 py-2 text-white duration-300 disabled:bg-gray-400',
+        classname,
+      )}
+    >
+      {pending && (
+        <svg
+          className='-ml-1 mr-3 size-5 animate-spin text-white'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+        >
+          <circle
+            className='opacity-25'
+            cx='12'
+            cy='12'
+            r='10'
+            stroke='currentColor'
+            strokeWidth='4'
+          ></circle>
+          <path
+            className='opacity-75'
+            fill='currentColor'
+            d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+          ></path>
+        </svg>
+      )}
+      {children}
+    </button>
+  );
+}
