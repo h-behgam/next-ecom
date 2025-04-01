f5a722ce4936fd35f338f8dd9e68bf6542e776b6
Hadi Behgam <h.behgam@gmail.com>
2025-04-01 15:48:40 +0330

    Update add-to-cart.tsx

--- src/components/front/product/add-to-cart.tsx
+++ src/components/front/product/add-to-cart.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { AddToCartProps } from '@/types/products-type';
 import AddToCartButton from './AddToCartButton';
 
-export default function AddToCart({ name, image, price }: AddToCartProps) {
+export default function AddToCart({ id, name, image, price }: AddToCartProps) {
   return (
     /* product price */
     <div className='flex h-full min-w-80 basis-3/12 flex-col rounded-md border bg-slate-50 p-5'>
@@ -21,7 +21,7 @@ export default function AddToCart({ name, image, price }: AddToCartProps) {
       </div>
       <p className='py-4 text-green-500'>۱۰۰۰+ فروش در هفته گذشته</p>
       <div className='pt-5'>
-        <AddToCartButton classname={'bg-rose-500 w-full'}>
+        <AddToCartButton id={id} classname={'bg-rose-500 w-full'}>
           افزودن به سبد
         </AddToCartButton>
       </div>
